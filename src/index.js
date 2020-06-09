5eb585025b72b4791dd2bd925a7de4d8ed6f35fb
Grayson Bailey <grayson.d.bailey@gmail.com>
2020-06-09 21:04:15 +0200

    brought back the same functionality as the local form already has...

--- src/index.js
+++ src/index.js
@@ -32,69 +32,48 @@ export const socket = io();
 
 
 export const overlay = new p5((p) => {
-  let x = 100;
-  let y = 100;
-  let can;
-
-
   let tFont;
   let curs;
-
-  let pointers = [];
-
+  let pointers = [p.createVector(0,0),p.createVector(0,0)];
   let cnv;
 
 
 
   p.preload = function() {
     tFont = p.loadFont("f7f26928c6b1edc770c616475459ecc8.otf");
-
-
   }
 
 
 
   p.setup = function() {
     cnv = p.createCanvas(p.windowWidth, p.windowHeight)
-
     console.log("setting up")
     p.textFont(tFont)
+    p.cursor("228ed835800150758bdcfe3a458531a8.png")
     socket.on('mouseRep', p.newDrawing)
-
-    p.refresh()
+    socket.on('unit', p.logUnit)
     p.fill(255)
   }
 
-  p.refresh = function() {
 
-
-    p.cursor("228ed835800150758bdcfe3a458531a8.png");
-
-    pointers = [p.createVector(0, 0), p.createVector(0, 0)]
-
-    p.translate(0,position);
-  }
-
-  p.draw = function(){
-
-
-  }
-
-  p.windowResized = function() {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
-    p.refresh();
-    console.log("resized!")
+  p.logUnit = function(data) {
+    if (data.db == discourses.db) {
+      discourses.addUnit(data.c, data.p, data.t, data.u, data.r, data.d, data.db)
+      discourses.vis()
+    }
   }
 
 
   p.newDrawing = function(data) {
     console.log("got sent something")
-    p.noStroke();
-    p.fill(255, 0, 100);
-    p.ellipse(data.x, data.y,3,3);
+    p.noFill();
+    p.stroke(255, 0, 100);
+    p.strokeWeight(1.5);
+    p.line(data.x - 3, data.y - 3, data.x + 3, data.y + 3)
+    p.line(data.x - 3, data.y + 3, data.x + 3, data.y - 3)
   }
 
-  p.printItOut = function(data){
+  p.printItOut = function(data) {
     console.log(data);
   }
 
@@ -111,30 +90,72 @@ export const overlay = new p5((p) => {
   }
 
   p.mouseClicked = function() {
-    if (pointers[0].x + pointers[0].y == 0) {
-      pointers[0].x = p.mouseX
-      pointers[0].y = p.mouseY
-      // console.log("POINTER ONE" + pointers[0])
-    } else {
-      pointers[1].x = p.mouseX
-      pointers[1].y = p.mouseY
-      // console.log("POINTER TWO" + pointers[1])
-      p.stroke('#ffA908');
-      p.strokeWeight(1);
-      p.line(pointers[0].x, pointers[0].y, pointers[1].x, pointers[1].y);
-      pointers[0] = p.createVector(0, 0);
+    if (document.getElementById('rp-b').classList.contains('current')) {
+      discourses.concern()
     }
   }
 
+  p.submitUnit = function() {
+    let temp = document.getElementById('tempGeist')
+    let tempButton = document.getElementById('tempGeistButton')
+    let escButton = document.getElementById('escapeGeistButton')
+    if (temp.value != "") {
+      let ttop = temp.offsetTop
+      let tleft = temp.offsetLeft
+      let tcont = temp.value
+      let tDisc = {
+        c: tcont,
+        p: {
+          x: tleft,
+          y: ttop - position
+        },
+        t: 0,
+        u: discourses.set.length,
+        r: [],
+        d: [p.year(), p.month(), p.day(), p.hour(), p.minute(), p.second()],
+        db: document.getElementById('filterKey').textContent
+      }
+      console.log(tDisc.d)
+      temp.remove()
+      tempButton.remove()
+      escButton.remove()
+      discourses.addUnit(tDisc.c, tDisc.p, tDisc.t, tDisc.u, [], tDisc.d, tDisc.db)
+      socket.emit('unit', tDisc);
+      discourses.vis()
+    } else {
+      temp.placeholder = '!!! empty unit cannot be submitted \r\n \r\n fill the area with discursive content \r\n \r\n ...or press ESCAPE to remove the input area'
+    }
+  }
 
-  p.mouseWheel = function(event){
-  p.refresh();
+  p.escapeUnit = function() {
+    let temp = document.getElementById('tempGeist')
+    let tempButton = document.getElementById('tempGeistButton')
+    let tempEscButton = document.getElementById('escapeGeistButton')
+    if (temp != null) {
+      temp.remove()
+      tempButton.remove()
+      tempEscButton.remove()
+    }
   }
 
   p.keyPressed = function() {
-    if(p.keyCode == p.ESCAPE){
-      let temp = document.getElementById('tempGeist')
-      if( temp != null){temp.remove()}
+    if (p.keyCode === 32) {
+      if (document.getElementById('gp-b').classList.contains('current')) {
+        if (document.getElementsByClassName('geist').length < 1) {
+          let input = p.createElement("textarea").class('geist')
+          let inputButton = p.createButton('submit').class('geistButton')
+          let escButton = p.createButton('X').class('geistButton')
+          inputButton.position(p.mouseX, p.mouseY - 20)
+          escButton.position(p.mouseX + 380, p.mouseY - 20)
+          input.position(p.mouseX, p.mouseY)
+          input.id('tempGeist')
+          input.attribute('placeholder', '. r/ ____ provide for response \r\n. q/ ____ provide for quotation \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n \r\n. // ____ provide before citation')
+          inputButton.id('tempGeistButton')
+          escButton.id('escapeGeistButton')
+          inputButton.mousePressed(p.submitUnit)
+          escButton.mousePressed(p.escapeUnit)
+        }
+      }
     }
   }
 
@@ -146,18 +167,28 @@ export const overlay = new p5((p) => {
     if (p.mouseX > 0 && p.mouseX < 100 && p.mouseY > 0 && p.mouseY < 100) {
       let fs = p.fullscreen();
       p.fullscreen(!fs);
-    } else if(document.getElementsByClassName('geist').length < 1){
-      let input = p.createElement('textarea').class('geist');
-        input.position(p.mouseX,p.mouseY)
-        input.id('tempGeist')
     }
   }
-
-
-
-
 }, 'overlay')
 
+$(document).on('keydown', '.geist', function() {
+  checkInput()
+})
+
+let checkInput = function() {
+  let el = document.getElementById("tempGeist")
+  if (el != null) {
+    let tempText = el.value
+    if (tempText.includes("r/")) {
+      el.classList.remove('quotation')
+      el.classList.add('response');
+
+    } else if (tempText.includes("q/")) {
+      el.classList.remove('response');
+      el.classList.add('quotation')
+    }
+  }
+}
 
 let reposition = function(event) {
   content.clear()
@@ -179,17 +210,19 @@ function loadDiscourseUnitsToArray(units) {
 
 window.onload = function() {
 
-  document.getElementById('overlay').addEventListener("wheel", event => reposition(event), {passive: true});
-  socket.on('dataRep', data =>{
+  document.getElementById('overlay').addEventListener("wheel", event => reposition(event), {
+    passive: true
+  });
+  socket.on('dataRep', data => {
     loadDiscourseUnitsToArray(data)
 
     console.log(discourses);
   })
 
   document.getElementById('vert30').onclick = () => {
-      vertSpeed = 30;
-      console.log(vertSpeed)
-    }
+    vertSpeed = 30;
+    console.log(vertSpeed)
+  }
   document.getElementById('vert60').onclick = () => {
     vertSpeed = 60;
     console.log(vertSpeed)
@@ -210,41 +243,41 @@ window.onload = function() {
 
 
   document.getElementById('rp-b').onclick = () => {
-      document.getElementById('gp-b').classList.remove('current');
-      document.getElementById('rp-b').classList.add('current');
-      switchModeInstructions(2)
-      discourses.vis()
-    }
-    document.getElementById('gp-b').onclick = () => {
-      document.getElementById('rp-b').classList.remove('current');
-      document.getElementById('gp-b').classList.add('current');
-      switchModeInstructions(1)
-      discourses.vis()
-    }
-    document.getElementById('discourseLoad').onclick = () => {
+    document.getElementById('gp-b').classList.remove('current');
+    document.getElementById('rp-b').classList.add('current');
+    switchModeInstructions(2)
+    discourses.vis()
+  }
+  document.getElementById('gp-b').onclick = () => {
+    document.getElementById('rp-b').classList.remove('current');
+    document.getElementById('gp-b').classList.add('current');
+    switchModeInstructions(1)
+    discourses.vis()
+  }
+  document.getElementById('discourseLoad').onclick = () => {
 
-        let presenter = new discursiveOverlay(overlay)
-        presenter.giveChoices()
+    let presenter = new discursiveOverlay(overlay)
+    presenter.giveChoices()
 
-    }
-    document.getElementById('switchLoad').onclick = () => {
-      document.getElementById('rp-b').classList.remove('current')
-      document.getElementById('gp-b').classList.remove('current')
-      document.getElementById('gp-b').classList.toggle('away')
-      document.getElementById('rp-b').classList.toggle('away')
-      document.getElementById('filterKey').textContent = "--"
-      content.clear()
-      switchModeInstructions(0)
-      let presenter = new discursiveOverlay(overlay)
-      overlay.clear()
-      presenter.giveChoices()
-      position = 0
-      document.getElementById('vertPos').innerText = position
-    }
+  }
+  document.getElementById('switchLoad').onclick = () => {
+    document.getElementById('rp-b').classList.remove('current')
+    document.getElementById('gp-b').classList.remove('current')
+    document.getElementById('gp-b').classList.toggle('away')
+    document.getElementById('rp-b').classList.toggle('away')
+    document.getElementById('filterKey').textContent = "--"
+    content.clear()
+    switchModeInstructions(0)
+    let presenter = new discursiveOverlay(overlay)
+    overlay.clear()
+    presenter.giveChoices()
+    position = 0
+    document.getElementById('vertPos').innerText = position
+  }
 
 
 
-    let datas = "frisk"
-    socket.emit('gimmeData', datas);
+  let datas = "frisk"
+  socket.emit('gimmeData', datas);
 
 }
