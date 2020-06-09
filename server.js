9504f92ec85ccf5c18c7a20cffed4b76db82a615
Grayson Bailey <grayson.d.bailey@gmail.com>
2020-06-09 16:57:28 +0200

    switched to on server

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const fetch = require('node-fetch')
 
 var app = express();
 app.use(express.static('dist'));
-//var server = app.listen(process.env.PORT || 8080);
-var server = app.listen(8081);
+var server = app.listen(process.env.PORT || 8080);
+//var server = app.listen(8081);
 var io = socket(server);
 console.log("My socket server is running Mboy");
 
